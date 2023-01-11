5b0028a8420314a27dda646dd4cd869a76d2424f
CHANDRASHEKHAR MESHRAM <>
2023-01-11 23:08:48 +0000

    heros using loop

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 /*
+ex13: put a list of questions together
+// hint: there's an error in these objects
+// if you copy/paste blindly it won't work
+questionOne = {
+  question: "Who is my favorite superhero?"
+  answer: "Dhruv"
+}
 
+questionTwo = {
+  question: "Which is my favorite sad song?"
+  answer: "Channa Meraya"
+}
 */
+
+var superMan = {
+  power: 1000,
+  skill: 900,
+  fly: true
+}
+
+var batMan = {
+  power: 500,
+  skill: 1000,
+  fly: false
+}
+let heros = [superMan, batMan];
+
+for (let i = 0; i < heros.length; i++) {
+    console.log(heros[i].power);
+    console.log(heros[i].skill);
+}
+
+// console.log("superMan power ", superMan.power, "batman power ", batMan.power);
