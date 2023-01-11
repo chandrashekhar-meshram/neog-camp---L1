264e1085f4ded0381407f8bf58df0ffee2922abc
CHANDRASHEKHAR MESHRAM <>
2023-01-11 20:34:36 +0000

    ex05: print right/wrong if greater than 25

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-// ex02: read the name of your user
+/* ex05: print right/wrong if greater than 25
+challenge
+Ask your user if your age is greater than 25 (or any number) and console right/wrong based on the answer.
+
+understand
+branching of code, i.e. only one branch runs based on a CONDITION
+*/
 
 var readlineSync = require("readline-sync");
 
-var userName = readlineSync.question("What is your name? ");
-console.log("Hi " + userName +" !");
+var answer = readlineSync.question("Do you know, my age? ");
+console.log("Answer " + answer);
+
+var count = 0;
+if(answer === "25" ){
+  console.log("right");
+  count++;
+} else {
+  console.log("wrong");
+}
+console.log("score = "+count);
