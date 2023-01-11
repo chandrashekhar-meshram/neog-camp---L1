d21efaeda20168b40049a0a92a3967d6a958229a
CHANDRASHEKHAR MESHRAM <>
2023-01-11 20:49:44 +0000

    ex07: function to add two numbers

---
+++ ex5.js
@@ -0,0 +1,21 @@
+/* ex05: print right/wrong if greater than 25
+challenge
+Ask your user if your age is greater than 25 (or any number) and console right/wrong based on the answer.
+
+understand
+branching of code, i.e. only one branch runs based on a CONDITION
+*/
+
+// var readlineSync = require("readline-sync");
+
+// var answer = readlineSync.question("Do you know, my age? ");
+// console.log("Answer " + answer);
+
+// var count = 0;
+// if(answer === "25" ){
+//   console.log("right");
+//   count++;
+// } else {
+//   console.log("wrong");
+// }
+// console.log("score = "+count);
