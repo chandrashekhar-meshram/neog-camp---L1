3752a52c8e6c60637987da45860b9732f01e6992
CHANDRASHEKHAR MESHRAM <>
2023-01-11 20:17:46 +0000

    ex02: read the name of your user

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hi");
+// ex02: read the name of your user
+
+var readlineSync = require("readline-sync");
+
+var userName = readlineSync.question("What is your name? ");
+console.log("Hi " + userName +" !");
