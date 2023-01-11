03cac5a8fec9284ececde4f927e0eee6e2b4368d
CHANDRASHEKHAR MESHRAM <>
2023-01-11 23:28:09 +0000

    end game

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-/*
-ex13: put a list of questions together
-// hint: there's an error in these objects
-// if you copy/paste blindly it won't work
-questionOne = {
-  question: "Who is my favorite superhero?"
-  answer: "Dhruv"
+// end game
+var readlineSync = require("readline-sync");
+
+var score = 0;
+function play(question, answer){
+  var userInput = readlineSync.question(question);
+  if(userInput === answer){
+    console.log("Right !");
+    score++;
+  } else {
+    console.log("wrong !")
+  }
+  console.log("Your current score - ", score);
 }
 
-questionTwo = {
-  question: "Which is my favorite sad song?"
-  answer: "Channa Meraya"
-}
-*/
+var questions = [
+  {
+    question: "Do you know my name ? ",
+    answer: "chandrashekhar"
+  },
+  {
+    question: "Do you know my city name ? ",
+    answer: "gondia"
+  }
+];
 
-var superMan = {
-  power: 1000,
-  skill: 900,
-  fly: true
+for(let i = 0; i < questions.length; i++){
+  play(questions[i].question, questions[i].answer);
 }
-
-var batMan = {
-  power: 500,
-  skill: 1000,
-  fly: false
-}
-let heros = [superMan, batMan];
-
-for (let i = 0; i < heros.length; i++) {
-    console.log(heros[i].power);
-    console.log(heros[i].skill);
-}
-
-// console.log("superMan power ", superMan.power, "batman power ", batMan.power);
